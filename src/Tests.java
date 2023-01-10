7a0bba914aeadb05f774044e764f7a643c6eac3c
taltosiano <94299489+taltosiano@users.noreply.github.com>
2023-01-10 13:12:29 +0200

    Update Tests.java

--- src/Tests.java
+++ src/Tests.java
@@ -1,58 +1,54 @@
-//import org.junit.jupiter.api.Test;
-//import org.junit.platform.commons.logging.Logger;
-//import org.junit.platform.commons.logging.LoggerFactory;
-
-import org.testng.annotations.Test;
-
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.logging.Logger;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import java.util.concurrent.*;
+import static java.lang.Thread.sleep;
 
 public class Tests {
-     public static final Logger logger = LoggerFactory.getLogger(Tests.class);
-        @Test
-        public void partialTest() {
-            CustomExecutor customExecutor = new CustomExecutor();
-            var task = Task.createTask(() -> {
-                int sum = 0;
-                for (int i = 1; i <= 10; i++) {
-                    sum += i;
-                }
-                return sum;
-            }, TaskType.COMPUTATIONAL);
-            var sumTask = customExecutor.submit(task);
-            final int sum;
-            try {
-                sum = sumTask.get(1, TimeUnit.MILLISECONDS);
-            } catch (InterruptedException | ExecutionException | TimeoutException e) {
-                throw new RuntimeException(e);
+    public static final Logger logger = LoggerFactory.getLogger(Tests.class);
+    @Test
+    public void partialTest(){
+        CustomExecutor customExecutor = new CustomExecutor();
+        Task task = Task.createTask(()->{
+            int sum = 0;
+            for (int i = 1; i <= 10; i++) {
+                sum += i;
             }
-            logger.info(() -> "Sum of 1 through 10 = " + sum);
-            Callable<Double> callable1 = () -> {
-                return 1000 * Math.pow(1.02, 5);
-            };
-            Callable<String> callable2 = () -> {
-                StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-                return sb.reverse().toString();
-            };
-// var is used to infer the declared type automatically
-            var priceTask = customExecutor.submit(() -> {
-                return 1000 * Math.pow(1.02, 5);
-            }, TaskType.COMPUTATIONAL);
-            var reverseTask = customExecutor.submit(callable2, TaskType.IO);
-            final Double totalPrice;
-            final String reversed;
-            try {
-                totalPrice = priceTask.get();
-                reversed = reverseTask.get();
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException(e);
-            }
-            logger.info(() -> "Reversed String = " + reversed);
-            logger.info(() -> String.valueOf("Total Price = " + totalPrice));
-            logger.info(() -> "Current maximum priority = " +
-                    customExecutor.getCurrentMax());
-            customExecutor.gracefullyTerminate();
-        }}
+            return sum;
+        }, TaskType.COMPUTATIONAL);
+        Future sumTask = customExecutor.submit(task);
+        final int sum;
+        try {
+            sum = (int) sumTask.get(1, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+        logger.info(()-> "Sum of 1 through 10 = " + sum);
+        Callable<Double> callable1 = ()-> {
+            return 1000 * Math.pow(1.02, 5);
+        };
+        Callable<String> callable2 = ()-> {
+            StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            sleep(1000);
+            return sb.reverse().toString();
+        };
+        // var is used to infer the declared type automatically
+        Future priceTask = customExecutor.submit(()-> {
+            return 1000 * Math.pow(1.02, 5);
+        }, TaskType.COMPUTATIONAL);
+        Future reverseTask = customExecutor.submit(callable2, TaskType.IO);
+        final Double totalPrice;
+        final String reversed;
+        try {
+            totalPrice = (Double) priceTask.get();
+            reversed = (String) reverseTask.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        logger.info(()-> "Reversed String = " + reversed);
+        logger.info(()->String.valueOf("Total Price = " + totalPrice));
+        logger.info(()-> "Current maximum priority = " +
+                customExecutor.getCurrentMax());
+        customExecutor.gracefullyTerminate();
+    }
+}
