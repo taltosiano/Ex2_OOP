68df757758347bd03219f2dd715419778c29c791
taltosiano <94299489+taltosiano@users.noreply.github.com>
2023-01-10 18:29:44 +0200

    Update Ex2_1.java

--- src/Ex2_1.java
+++ src/Ex2_1.java
@@ -1,85 +1,143 @@
-import java.util.*;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.concurrent.*;
 
-import static java.lang.Runtime.getRuntime;
+public class Ex2_1 {
 
-public class CustomExecutor<V> extends ThreadPoolExecutor {
-    int [] thePriority;
-    int minNumOfThreads = getRuntime().availableProcessors()/2;
-    int maxNumOfThreads = getRuntime().availableProcessors() - 1;
-    private final PriorityBlockingQueue<Runnable> tasks = new PriorityBlockingQueue<>(minNumOfThreads, (task1, task2) -> ((Task)task1).compareTo((Task) task2)) ;
+    public static void main(String[] args) {
 
-    public CustomExecutor() {
-        super(getRuntime().availableProcessors()/2, getRuntime().availableProcessors() - 1, 300, TimeUnit.MILLISECONDS,
-                new PriorityBlockingQueue<>(getRuntime().availableProcessors()/2, (task1, task2) -> ((Task)task1).compareTo((Task) task2)));
-        this.thePriority = new int[10];
-    }
+        String[] fileNames = createTextFiles(12, (int) Math.random(), 10);
 
-//    public void submit(Callable task, TaskType type) {
-//        Task currTask = new Task();
-//        currTask.createTask(task, type);
-//        tasks.add(currTask);
-//    }
-//
-//    public void submit(Callable task) {
-//        Task currTask = new Task();
-//        currTask.createTask(task);
-//        tasks.add(currTask);
-//    }
+            System.out.print("Total number of lines without thread: ");
+            long startTime = System.currentTimeMillis();
+            System.out.print(getNumOfLines(fileNames) + ".   ");
+            long endTime = System.currentTimeMillis();
+            NumberFormat formatter = new DecimalFormat("#0.00000");
+            System.out.print("Time: " + formatter.format((endTime - startTime) / 1000d) + " seconds" + "\n");
 
-//    public Future<V> submit(Task<V> task) {
-//        FutureTask futureTask = new FutureTask(task);
-//        tasks.add(futureTask);
-//        return futureTask;
-//    }
-//
-//    public Future<V> submit(Callable<V> callable, TaskType type) {
-//        Task<V> task = Task.createTask(callable, type);
-//        return submit(task);
-//    }
+            System.out.print("Total number of lines with thread: ");
+            startTime = System.currentTimeMillis();
+            System.out.print(getNumOfLinesThreads(fileNames) + ".      ");
+            endTime = System.currentTimeMillis();
+            System.out.print("Time: " + formatter.format((endTime - startTime) / 1000d) + " seconds" + "\n");
 
-    public Future<V> submit (Task t)
-    {
-        thePriority[t.type.getPriorityValue()]++;
-        return super.submit(t);
-    }
-
-    public  Future<V> submit (Callable t , TaskType taskType)
-    {
-        Callable task = Task.createTask(t,taskType);
-        return submit(task);
-    }
-
-    public Future<V> submit (Callable t)
-    {
-        Callable task = Task.createTask(t);
-        return submit((Task)task);
-    }
-
-//    public String getCurrentMax() {
-//        return "Current maximum priority =" + max;
-//    }
-public int getCurrentMax()
-{
-        for (int i = 1; i < 10; i++) {
-            if (thePriority[i] > 0)
-                return i;
+            System.out.print("Total number of lines with threadPool: ");
+            startTime = System.currentTimeMillis();
+            System.out.print(getNumOfLinesThreadPool(fileNames) + ".  ");
+            endTime = System.currentTimeMillis();System.out.print("Time: " + formatter.format((endTime - startTime) / 1000d) + " seconds" + "\n");
         }
-    return 0;
+
+
+    public static String[] createTextFiles(int n, int seed, int bound) {  // n files,
+        Random rand = new Random(seed);
+        String[] file_names = new String[n];      // Initializing a String array that will be returned with size of: number of files.
+
+        String folderPath = "./TheOutPut_files";//name of folder to store the files in it
+        File folder = new File(folderPath);
+        folder.mkdir();
+
+        try {
+            for (int i = 1; i <= n; i++) {   // writing n files
+                String fileName = "file_" + i + ".txt";
+                String filePath = folderPath + "/" + fileName;
+                int rows_num = rand.nextInt(bound);
+
+                FileWriter fileW = new FileWriter(filePath);
+                //       FileWriter fileW = new FileWriter("./src/files_i/file_" + i + ".txt");
+                file_names[i - 1] = "file_" + i + ".txt";
+
+                for (int j = 0; j < rows_num; j++)
+                    fileW.write("Hello World\n");
+                fileW.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file_names;
+    }
+
+    public static int getNumOfLines(String[] fileNames) {
+    int ans = 0;
+        for (int i=0; i < fileNames.length; i++) {
+        File file = new File(fileNames[i]);
+            String folderPath = "./TheOutPut_files";
+            String filePath = folderPath + "/" + file;
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            Scanner scanner = new Scanner(fileReader);
+            while(scanner.hasNextLine()){
+                scanner.nextLine();
+                ans++;
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+        return ans;
 }
 
-    public void gracefullyTerminate() {
-        super.shutdown();
+    public static int getNumOfLinesThreads(String[] fileNames) {
+    //  NumOfLinesThreads thread = new NumOfLinesThreads(fileNames.toString());
+        NumOfLinesThreads[] threads = new NumOfLinesThreads[fileNames.length];
+        int numOfLines = 0;
+        try {
+        for (int i = 0; i < fileNames.length; i++) {
+            threads[i] = new NumOfLinesThreads(fileNames[i]);
+            threads[i].start();
+            threads[i].join();
+        }
+        for (NumOfLinesThreads thread : threads) {
+                numOfLines += thread.getNumRows();
+            }
+        } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        return numOfLines;
     }
 
-    @Override
-    protected void beforeExecute(Thread t, Runnable r) {
-        boolean exist=false;
-        for(int i=0; i < thePriority.length && !exist; i++){
-            if(thePriority[i]>0){
-                thePriority[i]= thePriority[i]-1;
-                exist =true;
+//        int counter = 0;
+//        for (int i = 0; i < fileNames.length; i++) {
+//            NumOfLinesThreads l = new NumOfLinesThreads(fileNames[i]);
+//            l.start();
+//            try {
+//                l.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            counter += l.getNumRows();
+//        }
+//
+//        return counter;
+//    }
+
+    public static int getNumOfLinesThreadPool(String[] fileNames) {
+        ExecutorService executorService = Executors.newFixedThreadPool(fileNames.length);
+        List<Callable<Integer>> tasks = new ArrayList<>();
+        int numOfLines = 0;
+            for (String fileName : fileNames) {
+                tasks.add(new NumOfLinesThreadPool(fileName));
             }
+            List<Future<Integer>> numInOneCall = null;
+        try {
+            numInOneCall = executorService.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        for (Future<Integer> rows : numInOneCall)
+            try {
+                numOfLines += rows.get();
+            } catch (InterruptedException | ExecutionException e){
+                e.printStackTrace();
+            }
+
+        executorService.shutdown();
+        return numOfLines;
     }
 }
