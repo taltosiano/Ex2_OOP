4352added3cd6ec52b83e478585254a936f8ed1b
taltosiano <94299489+taltosiano@users.noreply.github.com>
2023-01-12 14:41:36 +0200

    Update MyFuture.java

--- src/Ex2_2/MyFuture.java
+++ src/Ex2_2/MyFuture.java
@@ -1,25 +1,15 @@
-package Ex2_2; /**
- * This is an Adapter class, which extends the FutureTask class,
- * and implements the Comparable interface.
- * The main purpose of this class is to allow the Ex2_2.Task objects,
- * which are passed to the submit method of the Ex2_2.CustomExecutor class,
- * to be used as elements in the priority queue that is used by the thread pool.
+package Ex2_2;
+/**
+ The purpose of the department is to coordinate between 2 departments. The class extends the FutureTask class and implements the Comparable interface.
+ The class allow the Task objects to be used as elements in the priority queue which used by the thread pool.
+ The task objects are passed to the submit method of the CustomExecutor class.
  */
 
-
-import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
 public class MyFuture<V> extends FutureTask implements Comparable<MyFuture<V>> {
 
     private final Task<V> task;
-
-
-    public MyFuture(Callable callable) {
-        super(callable);
-        this.task = null;
-    }
-
 
     public MyFuture(Task<V> task) {
         super(task.getTask());
@@ -29,11 +19,7 @@ public class MyFuture<V> extends FutureTask implements Comparable<MyFuture<V>> {
     public Task<V> getTask() {
         return task;
     }
-
-    public Callable<V> getCallable() {
-        return this.task.getTask();
-    }
-
+    
     /**
      * @return the priority of the task
      */
@@ -42,19 +28,13 @@ public class MyFuture<V> extends FutureTask implements Comparable<MyFuture<V>> {
     }
 
     /**
-     * This method overrides the method from Comparable , we compare two objects of type "Ex2_2.MyFuture" based
-     * on their task's priority
-     * @param other the object of type Ex2_2.MyFuture we compare
-     * @return 1 if this task has higher priority than the other task, -1 if this task has lower priority than the other task
-     * 0 if the two tasks have the same priority
+     * The method comparing to objects in type MyFuture
+     * @param other the other object of type MyFuture we want to compare to ours.
+     * @return 0 if the two tasks have the same priority, 1 if this task has higher priority than the other task and
+     * -1 if this task has lower priority than the other task
      */
-
     @Override
     public int compareTo(MyFuture<V> other) {
-
         return task.compareTo(other.getTask());
-//        return Integer.compare(task.getType().getPriorityValue(),
-//                other.task.getType().getPriorityValue());
-
     }
 }
